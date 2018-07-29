f5e5499f3e5f1379b3b46f61bda7cdccfce5bfb9
ANDROID-DEVILOP\cemil <cemilyesil@hotmail.com.tr>
2018-07-29 10:03:21 +0300

    örnek php boşlukjlar alındı

--- PHP JSON ORNEK/index.php
+++ PHP JSON ORNEK/index.php
@@ -1,29 +1,19 @@
 <?php
 $user="demo";
 $pass="demo";
-if(isset($_POST["user"])){
-
-
+if(isset($_POST["user"])){ 
 	$postuser=xss_clear("user");
 	$postpass=xss_clear("pass");
-	$postparam=xss_clear("param");
-
-
-	if($user==$postuser && $pass==$postpass){
-
-
+	$postparam=xss_clear("param"); 
+	if($user==$postuser && $pass==$postpass){ 
 		require ("Service.php");
 
 		$data=new Service();
 
 		if($postparam=="favorite"){
 
-			echo $data->favorite();
-
-
-		} elseif($postparam=="category"){
-
-
+			echo $data->favorite(); 
+		} elseif($postparam=="category"){ 
 			echo $data->category();
 
 		} elseif($postparam=="categoryItems"){
@@ -35,22 +25,11 @@ if(isset($_POST["user"])){
 		} else {
 
 			echo json_encode(array("msg"=>"Parametre Hatalı"));
-		}
-
-
-
-
-
-	} else {
-
-
-		echo json_encode(array("msg"=>"Kullanıcı Bilgileri Hatalı"));
-
-
+		} 
+	} else { 
+		echo json_encode(array("msg"=>"Kullanıcı Bilgileri Hatalı")); 
 	}
-}
-
-
+} 
 function xss_clear($par){
 	return htmlspecialchars(addslashes(trim($_POST[$par])));
 }
